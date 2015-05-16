306fbd4152829b01dcfde57e85c3d83e43b69774
wayofthepie <wayofthepie@gmail.com>
2015-05-16 15:48:13 +0000

    Basic call to list courses

--- src/Coursera.hs
+++ src/Coursera.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE
+    DeriveGeneric
+    , OverloadedStrings
+    #-}
 
 module Coursera where
 
+import Control.Lens.Operators
+import Data.Aeson
+import Data.Aeson.Lens
+import Data.Text
+import GHC.Generics
 import Network.Wreq
 
-test = get "https://api.coursera.org/api/catalog.v1/courses"
+
+data Courses = Courses
+    { elements :: [Course]
+    } deriving (Eq, Generic, Show)
+
+
+data Course = Course
+    { id  :: Int
+    , shortName :: Text
+    , name      :: Text
+    , links     :: Object -- FIXME!!!
+    } deriving (Eq, Generic, Show)
+
+instance FromJSON Courses
+instance FromJSON Course
+
+
+-- | List all courses
+courses :: IO (Maybe Courses)
+courses = do
+    r <- asJSON =<< get "https://api.coursera.org/api/catalog.v1/courses" :: IO (Response Courses)
+    return $ r ^? responseBody
