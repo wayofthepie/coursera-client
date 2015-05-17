c40e91304a4e84bf62f67e055622b868c5570ccd
wayofthepie <wayofthepie@gmail.com>
2015-05-17 17:38:46 +0000

    Client base url now configurable

--- src/Coursera.hs
+++ src/Coursera.hs
@@ -6,12 +6,21 @@
 module Coursera where
 
 import Control.Lens.Operators
+import Control.Monad.IO.Class (liftIO, MonadIO)
+import Control.Monad.Reader (ask, runReaderT, ReaderT)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Resource (runResourceT, ResourceT)
 import Data.Aeson
 import Data.Aeson.Lens
-import Data.Text
+import qualified Data.Text as T
 import GHC.Generics
 import Network.Wreq
 
+type CourseraClient a = ReaderT CourseraClientConfig IO a
+
+data CourseraClientConfig = CourseraClientConfig
+    { apiBase :: T.Text
+    } deriving (Eq, Show)
 
 data Courses = Courses
     { elements :: [Course]
@@ -20,17 +29,25 @@ data Courses = Courses
 
 data Course = Course
     { id  :: Int
-    , shortName :: Text
-    , name      :: Text
+    , shortName :: T.Text
+    , name      :: T.Text
     , links     :: Object -- FIXME!!!
     } deriving (Eq, Generic, Show)
 
 instance FromJSON Courses
 instance FromJSON Course
 
+runCourseraClient ::MonadIO m =>  CourseraClientConfig -> CourseraClient a -> m a
+runCourseraClient config action =
+    liftIO $ runReaderT action config
 
 -- | List all courses
-courses :: IO (Maybe Courses)
+courses :: CourseraClient (Maybe Courses)
 courses = do
-    r <- asJSON =<< get "https://api.coursera.org/api/catalog.v1/courses" :: IO (Response Courses)
-    return $ r ^? responseBody
+    cfg <- ask
+    resp<- getCoursesJson $ apiBase cfg
+    return $ resp ^? responseBody
+  where
+    getCoursesJson :: T.Text -> CourseraClient (Response Courses)
+    getCoursesJson baseUrl = lift $
+        asJSON =<< get (T.unpack baseUrl ++ "/catalog.v1/courses")
